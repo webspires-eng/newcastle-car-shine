f4ce87cbe023b14256de079d80ff791ce1b9b2f3
khyberwear <atifjan2019@gmail.com>
2026-02-10 09:48:59 +0000

    Add ts-nocheck to Deno edge function

--- supabase/functions/send-inquiry-email/index.ts
+++ supabase/functions/send-inquiry-email/index.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck - This file runs on Deno (Supabase Edge Functions), not Node.js
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 
